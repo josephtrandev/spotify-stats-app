dce882194ada20a2bc3355284ef288b09c1fa7eb
Joseph <56847381+josephtrandev@users.noreply.github.com>
2024-10-11 13:55:28 -0500

    Created react app and updated callback to pass tokens

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const accessToken = urlParams.get('access_token');
+    const refreshToken = urlParams.get('refresh_token');
+
+    console.log(accessToken);
+    console.log(refreshToken);
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="http://localhost:8888/login"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Log in to Spotify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- index.js
+++ index.js
@@ -73,28 +73,24 @@ app.get('/callback', (req, res) => {
     },
   })
     .then(response => {
-      if (response.status === 200) {
-        
-        const { access_token, token_type } = response.data;
+      if (response.status === 200) {  
+        const { access_token, refresh_token } = response.data;
 
-        const { refresh_token } = response.data;
+        const queryParams = querystring.stringify({
+            access_token,
+            refresh_token,
+        });
 
-        axios.get(`http://localhost:8888/refresh_token?refresh_token=${refresh_token}`)
-            .then(response => {
-                res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-            })
-            .catch(error => {
-                res.send(error);
-            });
+        res.redirect(`http://localhost:3000/?${queryParams}`);
 
       } else {
-        res.send(response);
+        res.redirect(`/${querystring.stringify({ error: 'invalid_token' })}`);
       }
     })
     .catch(error => {
       res.send(error);
     });
-})
+});
 
 app.get('/refresh_token', (req, res) => {
     const { refresh_token } = req.query;
